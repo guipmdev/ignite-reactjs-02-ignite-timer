ee31640c58804ebad6c60b74839b4bba22b63f7b
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-06-22 10:34:33 -0300

    Estrutura da aplicação - Styled Components

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,25 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerPros {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerPros>`
+  width: 100px;
+  height: 40px;
+
+  ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `;
+  }}
+`;
