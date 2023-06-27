31a1f8f070038cc98f2d42e565007dfc16e14b39
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-06-27 10:45:29 -0300

    Reducers - Criando reducer de ciclos

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -2,6 +2,9 @@ import { HistoryContainer, HistoryList, Status } from './styles'
 
 import { useContext } from 'react'
 
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { CyclesContext } from '../../contexts/CyclesContext'
 
 export function History() {
@@ -28,7 +31,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
